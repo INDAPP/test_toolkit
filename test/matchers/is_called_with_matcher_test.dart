db316b0a5f9f98f41f2537b3f37e674202595657
Riccardo Pizzoni <riccardo.pizzoni@welld.ch>
2023-05-05 23:35:25 +0200

    test: is called with matcher

---
+++ test/matchers/is_called_with_matcher_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:test_toolkit/matchers/is_called_with_matcher.dart';
+import 'package:test_toolkit/mocked_function.dart';
+
+main() {
+  group('isCalledWith matcher', () {
+    test('should match a mocked function call with no arguments', () {
+      final function = MockedFunction();
+      function();
+      expect(function, isCalledWith([]));
+    });
+
+    test('should match a mocked function call with arguments', () {
+      final function = MockedFunction();
+      function('arg', 1);
+      expect(function, isCalledWith(['arg', 1]));
+    });
+
+    group('when there are multiple calls', () {
+      final function = MockedFunction();
+      function();
+      function(true, 1);
+      function('arg');
+
+      test('should match a mocked function call with arguments', () {
+        expect(function, isCalledWith([true, 1]));
+      });
+
+      test('should match a mocked function call with no arguments', () {
+        expect(function, isCalledWith([]));
+      });
+
+      test('should match a mocked function nTh call with arguments', () {
+        expect(function, isCalledWith(['arg'], nTh: 3));
+      });
+    });
+  });
+}
